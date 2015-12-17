e31749f3b6c8883e143d97dd07125ce7a15c998b
Chris Marshall <chris@chrismar035.com>
2015-12-17 08:11:24 -0500

    Day 4 - Puzzle 1

---
+++ day-4/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+)
+
+var secret = "yzbqklnj"
+
+func main() {
+	candidate := 1
+
+	for {
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(secret+strconv.Itoa(candidate))))
+
+		if stringStartsWithZeros(5, hash) {
+			fmt.Println("We did it!", candidate)
+			return
+		}
+
+		candidate++
+	}
+}
+
+func stringStartsWithZeros(leadingZeros int, input string) bool {
+	var zero = []byte("0")[0]
+	for _, b := range []byte(input)[:leadingZeros] {
+		if b != zero {
+			return false
+		}
+	}
+	return true
+}
