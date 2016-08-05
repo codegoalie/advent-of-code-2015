bcc7df73b410bc6e88e19f47d81ce553e97e3035
Chris Marshall <chris@chrismar035.com>
2016-08-05 14:53:40 -0400

    Setup day 10 for remote execution
    Basically can be run on a vm in the cloud and post
    the answers to slack

--- day-10/main.go
+++ day-10/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
 	"strconv"
 	"strings"
 )
 
+// Message is the heding text and attachments for the Slack message
+type Message struct {
+	Text string `json:"text"`
+}
+
 func main() {
 	seen := "1113122113"
-	for i := 0; i < 40; i++ {
+	for i := 0; i < 50; i++ {
 		seen = lookAndSee(seen)
 		fmt.Printf("i = %+v\n", i)
+		postToSlack(Message{Text: fmt.Sprintf("i = %+v\n", i)})
 	}
 	fmt.Printf("len(seen) = %+v\n", len(seen))
+	postToSlack(Message{Text: fmt.Sprintf("len(seen) = %+v\n", len(seen))})
 }
 
 func lookAndSee(in string) string {
@@ -32,4 +44,28 @@ func lookAndSee(in string) string {
 	}
 	saw += strconv.Itoa(count) + current
 	return saw
+}
+
+func postToSlack(message Message) {
+	b, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println(string(b))
+
+	resp, err := http.PostForm(os.Getenv("SLACK_URL"),
+		url.Values{"payload": {string(b)}})
+
+	if err != nil {
+		fmt.Println("Error posting to Slack", err)
+	} else {
+		rawBody, err := ioutil.ReadAll(resp.Body)
+		_ = resp.Body.Close()
+		if err != nil {
+			fmt.Println("failed to read Slack response", err)
+		} else {
+			fmt.Println("Slack response", string(rawBody))
+		}
+	}
 }
