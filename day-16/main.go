b92a904af8871ea9a8a65a815f25cf11d2083dfb
Chris Marshall <chris@chrismar035.com>
2016-08-11 13:06:56 -0400

    Day 16 - pt 1

---
+++ day-16/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type sue struct {
+	number int
+	attrs  map[string]int
+}
+
+func newSue() sue {
+	return sue{
+		attrs: map[string]int{},
+	}
+}
+
+func main() {
+	filename := "input.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file:", filename, err)
+	}
+
+	sues := []sue{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		splits := strings.SplitN(line, ": ", 2)
+
+		sue := newSue()
+		identifiers := strings.Split(splits[0], " ")
+
+		number, err := strconv.Atoi(string(identifiers[1]))
+		if err != nil {
+			panic(err)
+		}
+		sue.number = number
+		for _, attrString := range strings.Split(splits[1], ", ") {
+			attrs := strings.Split(attrString, ": ")
+			count, err := strconv.Atoi(attrs[1])
+			if err != nil {
+				panic(err)
+			}
+			sue.attrs[attrs[0]] = count
+		}
+
+		sues = append(sues, sue)
+	}
+
+	knowns := map[string]int{
+		"children":    3,
+		"cats":        7,
+		"samoyeds":    2,
+		"pomeranians": 3,
+		"akitas":      0,
+		"vizslas":     0,
+		"goldfish":    5,
+		"trees":       3,
+		"cars":        2,
+		"perfumes":    1,
+	}
+
+	for _, sue := range sues {
+		good := true
+		for attr, count := range sue.attrs {
+			if knowns[attr] != count {
+				good = false
+				break
+			}
+		}
+		if good {
+			fmt.Printf("sue.number = %+v\n", sue.number)
+		}
+	}
+}
